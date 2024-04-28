4f9d961a000677df05598a0ba938155bf82e00db
Jerryqjr <1317102375@qq.com>
2024-04-28 21:33:16 +0800

    ref对比reactive

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,12 +1,9 @@
 <template>
   <div class="person">
     <h2>汽车信息：一台{{ car.brand }}汽车，价值{{ car.price }}万</h2>
-    <h2>游戏列表：</h2>
-    <ul>
-      <li v-for="g in games" :key="g.id">{{ g.name }}</li>
-    </ul>
     <button @click="changeCarPrice">修改汽车价格</button>
-    <button @click="changeFirstGame">修改第一游戏</button>
+    <button @click="changeCarBrand">修改汽车品牌</button>
+    <button @click="changeCar">修改汽车</button>
   </div>
 </template>
 
@@ -14,17 +11,28 @@
 import { ref } from "vue"
 
 // 数据
+//1.let car = reative({ brand: "奔驰", price: 100 })
 let car = ref({ brand: "奔驰", price: 100 })
-let games = ref([
-  { id: "ahsgdyfa01", name: "英雄联盟" },
-  { id: "ahsgdyfa02", name: "王者荣耀" },
-  { id: "ahsgdyfa03", name: "原神" },
-])
+let sum = ref(0)
+
+//方法
 function changeCarPrice() {
   car.value.price += 10
 }
-function changeFirstGame() {
-  games.value[0].name = "CSGO"
+
+function changeCarBrand() {
+  car.value.brand = "宝马"
+}
+
+function changeCar() {
+  // car = { brand: "特斯拉", price: 20 } //这样写页面不更新
+  // car = reactive({ brand: "特斯拉", price: 20 }) //这样写页面也不更新
+
+  //下面这个写法页面可以更新
+  //Object.assign(car,{ brand: "特斯拉", price: 20 })
+
+  //使用ref定义car可以直接改value
+  car.value = { brand: "特斯拉", price: 20 }
 }
 </script>
 
