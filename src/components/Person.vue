8de18794472d3dc9ed5b9443c2a37f6c12cbee08
Jerryqjr <1317102375@qq.com>
2024-04-29 11:02:32 +0800

    toRefs 与 toRef

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,38 +1,35 @@
 <template>
   <div class="person">
-    <h2>汽车信息：一台{{ car.brand }}汽车，价值{{ car.price }}万</h2>
-    <button @click="changeCarPrice">修改汽车价格</button>
-    <button @click="changeCarBrand">修改汽车品牌</button>
-    <button @click="changeCar">修改汽车</button>
+    <h2>姓名：{{ person.name }}</h2>
+    <h2>年龄：{{ person.age }}</h2>
+    <h2>性别：{{ person.gender }}</h2>
+    <button @click="changeName">修改名字</button>
+    <button @click="changeAge">修改年龄</button>
+    <button @click="changeGender">修改性别</button>
   </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref } from "vue"
+import { ref, reactive, toRefs, toRef } from "vue"
 
 // 数据
-//1.let car = reative({ brand: "奔驰", price: 100 })
-let car = ref({ brand: "奔驰", price: 100 })
-let sum = ref(0)
+let person = reactive({ name: "Jerry", age: 18, gender: "男" })
 
-//方法
-function changeCarPrice() {
-  car.value.price += 10
+// 通过toRefs将person对象中的n个属性批量取出，且依然保持响应式的能力
+let { name, gender } = toRefs(person)
+
+// 通过toRef将person对象中的age属性取出，且依然保持响应式的能力
+let age = toRef(person, "age")
+
+// 方法
+function changeName() {
+  name.value += "~"
 }
-
-function changeCarBrand() {
-  car.value.brand = "宝马"
+function changeAge() {
+  age.value += 1
 }
-
-function changeCar() {
-  // car = { brand: "特斯拉", price: 20 } //这样写页面不更新
-  // car = reactive({ brand: "特斯拉", price: 20 }) //这样写页面也不更新
-
-  //下面这个写法页面可以更新
-  //Object.assign(car,{ brand: "特斯拉", price: 20 })
-
-  //使用ref定义car可以直接改value
-  car.value = { brand: "特斯拉", price: 20 }
+function changeGender() {
+  gender.value = "女"
 }
 </script>
 
