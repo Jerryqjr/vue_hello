a8999c556fd2d74fea54329f84339f0fef95a075
Jerryqjr <1317102375@qq.com>
2024-04-30 16:09:52 +0800

    修改vue语法检测为eslint

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts">
-import Person from "./components/Person.vue"
+import Person from "./components/PersonSum.vue"
 export default {
   name: "App", //组件名
   components: { Person }, //注册组件
